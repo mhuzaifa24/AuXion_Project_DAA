498f46076833b2427690d4fa3aa94e9b8571371b
mhuzaifa24 <149464955+mhuzaifa24@users.noreply.github.com>
2025-04-14 12:57:19 +0500

    Create auction_data.h

---
+++ code/auction_data.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+struct Item
+{
+    string item_id;
+    string name;
+    int base_price;
+};
+
+struct Bid 
+{
+    string bidder_id;
+    string item_id;
+    int amount;
+    time_t timestamp;
+};
+
