c96b902d16387fda75b563a7ef64fa2c8e015123
mhuzaifa24 <149464955+mhuzaifa24@users.noreply.github.com>
2025-04-23 10:32:51 +0500

    Styling bidder.cpp
    minimal changes that prints welcome message plus allows to run on vsstudio instead of cmd

--- code/Bidder_APP/bidder.cpp
+++ code/Bidder_APP/bidder.cpp
@@ -1,46 +1,59 @@
 #include "bidder.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    if (argc != 2) {
-        std::cerr << "Usage: Bidder.exe <Node_ID>\n";
-        return 1;
-    }
-
-    int nodeID = std::atoi(argv[1]);
+    
+    cout << "\n--------------------------------------    Hey Bidders! Welcome to AuXion    -----------------------------------------\n\n";
 
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+   
+    for (int nodeID = 1; nodeID <= MAX_BIDDERS; ++nodeID) 
+    {
 
-    sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+        
+        SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock == INVALID_SOCKET) 
+        {
+            cerr << "Socket creation failed for Node " << nodeID << "\n";
+            WSACleanup();
+            return 1;
+        }
 
-    if (connect(sock, (sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection to Coordinator failed\n";
+        sockaddr_in serv_addr;
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(PORT);
+        inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+       
+        if (connect(sock, (sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) 
+        {
+            cerr << "Connection to Coordinator failed for Node " << nodeID << "\n";
+            closesocket(sock);
+            continue; 
+        }
+
+        
+        cout << "\n[Node " << nodeID << "] Enter your bid amount: ";
+        int bid;
+        cin >> bid;
+
+       
+        ostringstream msg;
+        msg << "Node" << nodeID << ":Bid:" << bid;
+        send(sock, msg.str().c_str(), msg.str().length(), 0);
+
+        char buffer[1024] = { 0 };
+        int valread = recv(sock, buffer, sizeof(buffer), 0);
+        if (valread > 0) {
+            cout << "[Node " << nodeID << "] Coordinator says: " << buffer << "\n";
+        }
+
+
         closesocket(sock);
-        WSACleanup();
-        return 1;
     }
 
-    int bid;
-    std::cout << "[Node " << nodeID << "] Enter your bid amount: ";
-    std::cin >> bid;
-
-    std::ostringstream msg;
-    msg << "Node" << nodeID << ":Bid:" << bid;
-    send(sock, msg.str().c_str(), msg.str().length(), 0);
-
-    char buffer[1024] = { 0 };
-    int valread = recv(sock, buffer, sizeof(buffer), 0);
-    if (valread > 0) {
-        std::cout << "[Node " << nodeID << "] \n Coordinator says: " << buffer << "\n";
-    }
-
-    closesocket(sock);
     WSACleanup();
     return 0;
 }
